fc928ec756b6efbda810433843e3b628ca2ed7f9
Nandkishor Dhawale <107688392+drnmdhawale@users.noreply.github.com>
2022-07-04 11:29:12 +0530

    added project files related to java tutorial
    -u

---
+++ SerialTest/src/serialtest/SerialTest.java
@@ -0,0 +1,25 @@
+
+package serialtest;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.io.OutputStream;
+import gnu.io.CommPortIdentifier; 
+import gnu.io.SerialPort;
+import gnu.io.SerialPortEvent; 
+import gnu.io.SerialPortEventListener; 
+import java.util.Enumeration;
+
+/**
+ *
+ * @author Shree
+ */
+public class SerialTest {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
